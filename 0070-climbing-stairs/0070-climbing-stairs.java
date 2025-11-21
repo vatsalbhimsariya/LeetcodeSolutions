cd39144cd426ce445d8e6f024077e29813121155
vatsalbhimsariya <vatsalbhimsariya@gmail.com>
2025-11-22 02:35:11 +0530

    Time: 0 ms (100%), Space: 42 MB (22.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=0){
-            return 0;
+        if (n<=0) {return 0;}
+        if(n==1) {return 1;}
+        if (n == 2) {return 2;}
+        int prev1=1;
+        int prev2=2;
+        for(int i=3;i<=n;i++){
+            int ways=prev1+prev2;
+            prev1=prev2;
+            prev2=ways;
         }
-        int[]memo=new int[n+1];
-        if(n>=1){
-            memo[1]=1;
-        }
-        return fib(n,memo);
-    }
-    private static int fib(int n, int []memo){
-        if(n==0){
-            return 1;
-        }
-        if(memo[n]!=0){
-            return memo[n];
-        }
-        int result=fib(n-1,memo)+fib(n-2,memo);
-       memo[n]=result;
-    return result;
+        return prev2;
     }
 }
